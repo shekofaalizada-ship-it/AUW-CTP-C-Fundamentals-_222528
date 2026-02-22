efac9cab31d5b8460b35292bb1e62ab27b222cd4
shekofaalizada-ship-it <shekofa.alizada@auw.edu.bd>
2026-02-22 10:15:02 +0600

    Add files via upload

---
+++ q36_222528.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(void)
+{
+
+    int numbers[5] = {20, 30, 40, 50, 60};
+
+
+    printf("Array Elements: ");
+    for (int i = 0; i < 5; i++)
+    {
+        printf("%i ", numbers[i]);
+    }
+    printf("\n");
+}
