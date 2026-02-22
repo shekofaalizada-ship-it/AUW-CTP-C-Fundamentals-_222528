84bb956a8544bb9d75ba3266371ea28bab064ef2
shekofaalizada-ship-it <shekofa.alizada@auw.edu.bd>
2026-02-22 10:08:32 +0600

    Add files via upload

---
+++ q31_222528.c
@@ -0,0 +1,32 @@
+#include <cs50.h>
+#include <stdio.h>
+
+int main(void)
+{
+    int mark = get_int("Enter mark (0-100): ");
+
+    if (mark < 0 || mark > 100)
+    {
+        printf("Invalid Mark\n");
+    }
+    else if (mark >= 80)
+    {
+        printf("Grade A\n");
+    }
+    else if (mark >= 70)
+    {
+        printf("Grade B\n");
+    }
+    else if (mark >= 60)
+    {
+        printf("Grade C\n");
+    }
+    else if (mark >= 50)
+    {
+        printf("Grade D\n");
+    }
+    else
+    {
+        printf("Grade F\n");
+    }
+}
