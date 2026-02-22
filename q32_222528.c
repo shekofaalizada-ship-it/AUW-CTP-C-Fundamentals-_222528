0437004efd0e9a0e00a3acfa50cfcc99011ecea3
shekofaalizada-ship-it <shekofa.alizada@auw.edu.bd>
2026-02-22 10:18:05 +0600

    Add files via upload

---
+++ q32_222528.c
@@ -0,0 +1,28 @@
+#include <cs50.h>
+#include <stdio.h>
+
+int main(void)
+{
+    int age = get_int("Enter age: ");
+
+    if (age < 0)
+    {
+        printf("Invalid Age\n");
+    }
+    else if (age <= 12)
+    {
+        printf("Child\n");
+    }
+    else if (age <= 19)
+    {
+        printf("Teen\n");
+    }
+    else if (age <= 59)
+    {
+        printf("Adult\n");
+    }
+    else
+    {
+        printf("Senior\n");
+    }
+}
